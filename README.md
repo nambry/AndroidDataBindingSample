fcd9148a20f6b5aacdf0d1e86c066e7cf665ed38
南部竜太郎 <namb@namb.local>
2019-03-14 23:31:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# DataBindingがうまくいかない場合
+- tools:context=".MainActivity"が削除されていることを確認する
+- layoutタグで囲われていることを確認する
+- Build → Clean ProjectとRebuild Projectを行う
+- File → Invalidate Cashes/Restart...を行う
+
---
+++ app/src/main/java/com/nambry/android/androiddatabindingsample/User.java
@@ -0,0 +1,12 @@
+package com.nambry.android.androiddatabindingsample;
+
+public class User {
+    public final String firstName;
+
+    public final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
